a73b6263a9ba640c43c83d32c984f17da6df27db
civky <gladys.gonzalez.14@sansano.usm.cl>
2018-10-20 11:34:17 -0300

    Lista con errores

---
+++ app/src/main/java/com/example/ecreyes/ccompra/MarketList.java
@@ -0,0 +1,48 @@
+package com.example.ecreyes.ccompra;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MarketList extends AppCompatActivity {
+
+    ListView mylist;
+    String text[] = new String[]{"Hello",
+            "This",
+            "is",
+            "Custom",
+            "ListView",
+            "This",
+            "is",
+            "Custom",
+            "ListView",
+            "This",
+            "is",
+            "Custom",
+            "ListView",
+            "This"};
+    int image[] = new int[]{R.drawable.about,
+            R.drawable.boost,
+            R.drawable.send,
+            R.drawable.settings,
+            R.drawable.share,
+            R.drawable.boost,
+            R.drawable.send,
+            R.drawable.settings,
+            R.drawable.share,
+            R.drawable.share,
+            R.drawable.boost,
+            R.drawable.send,
+            R.drawable.settings,
+            R.drawable.share};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        mylist = (ListView) findViewById(R.id.mylist);
+        MyCustomListAdapter myadapter = new MyCustomListAdapter(getApplicationContext(),image, text);
+        mylist.setAdapter((ListAdapter) myadapter);
+
+    }
+}
