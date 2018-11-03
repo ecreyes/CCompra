f2f6ec6667452bc119c0e5fce317e28c52b0fb4d
civky <gladys.gonzalez.14@sansano.usm.cl>
2018-11-02 22:26:49 -0300

    listas tiendas

--- app/src/main/java/com/example/ecreyes/ccompra/ListaTiendasFragment.java
+++ app/src/main/java/com/example/ecreyes/ccompra/ListaTiendasFragment.java
@@ -3,12 +3,26 @@ package com.example.ecreyes.ccompra;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.example.ecreyes.ccompra.Objetos.FirebaseReferences;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ListaTiendasFragment extends Fragment {
@@ -16,6 +30,12 @@ public class ListaTiendasFragment extends Fragment {
     View v;
     //variables
     ListView mylist;
+
+    DatabaseReference refTienda = FirebaseDatabase.getInstance().getReference(FirebaseReferences.TIENDA_REFERENCES);
+    Query q_tiendas;
+
+    FirebaseStorage mstore;
+
     String text[] = new String[]{"Tienda 1",
             "Tienda 2",
             "Tienda 3",
@@ -58,7 +78,6 @@ public class ListaTiendasFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -71,8 +90,16 @@ public class ListaTiendasFragment extends Fragment {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_lista_tiendas, container, false);
         mylist = (ListView) v.findViewById(R.id.mylist);
-        MyCustomListAdapter myadapter = new MyCustomListAdapter(getContext(),image, text);
+        MyCustomListAdapter myadapter = new MyCustomListAdapter(getContext(), image, text);
         mylist.setAdapter((ListAdapter) myadapter);
+        //q_tiendas = refTienda.getRef((FirebaseReferences.TIENDA_REFERENCES);
+        final DatabaseReference TiendRef = FirebaseDatabase.getInstance().getReference("Tienda");
+        Log.i("query", TiendRef.orderByChild("nombre").toString());
+        getTiendasFromDB();
+
+        mstore = FirebaseStorage.getInstance();
+        Log.i("LISTA", "MALLL");
+
         return v;
     }
 
@@ -80,6 +107,34 @@ public class ListaTiendasFragment extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+    }
+
+    public void getTiendasFromDB(){
+
+        refTienda.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<String> nombresList = new ArrayList<String>();
+                List<String> imagenesList = new ArrayList<String>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //Tienda tienda = ds.getValue(Tienda.class);
+                    //tiendaList.add(tienda);
+                    //System.out.print(tienda);
+                    String Nombre = ds.child("nombre").getValue(String.class);
+                    String Imagen = ds.child("uri").getValue(String.class);
+                    Log.d("QUERY", "Imagen : " + Imagen + "   /" + Nombre + "/" );
+                    nombresList.add(Nombre);
+                    imagenesList.add(Imagen);
+
+                }
+                Log.i("Lista--", nombresList.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("QUERY", "ERRRRROR :c"            );
+            }
+        });
     }
 
     @Override
@@ -103,4 +158,6 @@ public class ListaTiendasFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
