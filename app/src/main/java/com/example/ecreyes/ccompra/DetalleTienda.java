97482aac5d5977b6ee43cbe9357022a71274a859
ecreyes <ereyesoyarzo@gmail.com>
2018-11-04 18:32:47 -0300

    al hacer click en una tienda se muestra el detalle y se cargan sus datos, se setea la pagina de inicio las de tiendas

--- app/src/main/java/com/example/ecreyes/ccompra/DetalleTienda.java
+++ app/src/main/java/com/example/ecreyes/ccompra/DetalleTienda.java
@@ -1,16 +1,20 @@
-package com.example.ecreyes.ccompra.Objetos;
+package com.example.ecreyes.ccompra;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.ecreyes.ccompra.R;
 
 public class DetalleTienda extends AppCompatActivity {
     TextView titulo;
-
+    TextView ubicacion;
+    TextView descripcion;
+    ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +26,20 @@ public class DetalleTienda extends AppCompatActivity {
 
         // getIntent() is a method from the started activity
         Intent myIntent = getIntent(); // gets the previously created intent
-        String firstKeyName = myIntent.getStringExtra("firstKeyName"); // will return "FirstKeyValue"
-        String secondKeyName= myIntent.getStringExtra("secondKeyName"); // will return "SecondKeyValue"
+        String keynombre = myIntent.getStringExtra("keynombre"); // will return "FirstKeyValue"
+        String keycategoria = myIntent.getStringExtra("keycategoria");
+        String keydescripcion = myIntent.getStringExtra("keydescripcion");
+        String keyuri = myIntent.getStringExtra("keyuri");
+        boolean keyestado = myIntent.getExtras().getBoolean("keyestado");
         titulo = findViewById(R.id.titulo_tienda);
-        titulo.setText(firstKeyName);
+        ubicacion = findViewById(R.id.lugar_tienda);
+        descripcion = findViewById(R.id.descripcion_tienda);
+        imagen = findViewById(R.id.imagen_tienda);
+        titulo.setText(keynombre);
+        descripcion.setText(keydescripcion);
+        Glide.with(this)
+                .load(keyuri)
+                .into(imagen);
     }
 
     @Override
